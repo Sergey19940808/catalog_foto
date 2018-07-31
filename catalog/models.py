ac404aee88edc755958e5ede3527e5de54425851
Sergey19940808 <alekseyserzh88@gmail.com>
2018-07-31 17:45:28 +0500

    rename attr models

--- catalog/models.py
+++ catalog/models.py
@@ -4,9 +4,9 @@ from django.db import models
 from smartfields import fields
 
 class Foto(models.Model):
-    image = fields.ImageField(upload_to='images', blank=True, verbose_name=u'Изображение:')
-    name = models.CharField(blank=False, max_length=100, unique=True, verbose_name=u'Имя изображения:')
-    text = models.TextField(verbose_name=u'Описание изображения:')
+    image = fields.ImageField(upload_to='images', blank=True, verbose_name=u'Фотография:')
+    name = models.CharField(blank=False, max_length=100, unique=True, verbose_name=u'Имя:')
+    text = models.TextField(verbose_name=u'Описание:')
     date_added = models.DateTimeField(default=django.utils.timezone.now, verbose_name=u'Дата публикации:')
 
     def __str__(self):
